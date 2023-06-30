8021ea2d5b567557a72a48d03caacd3b46410e39
Nhat Minh <nhatminh26209@gmail.com>
2023-07-01 01:01:20 +0700

    .

--- connect.js
+++ connect.js
@@ -25,24 +25,24 @@ app.get('/database', (req,res)=>{
     })
 })
 
-// app.post('/database', (req,res)=>{
-//     console.log(req.body)
-//     var data = {
-//         UserName: req.body.UserName,
-//         PassWord: req.body.PassWord,
-//         idUser: req.body,idUser
-//     }
-//     var sql = 'insert into User set ?'
-//     db.query(sql,data,(err,kq)=>{
-//         if(err) throw err
-//         console.log(kq)
-//         res.send({
-//             status: "Them thanh cong",
-//             UserName: req.body.UserName,
-//             PassWord: req.body.PassWord
-//         })
-//     })
-// })
+app.post('/database', (req,res)=>{
+    console.log(req.body)
+    var data = {
+        UserName: req.body.UserName,
+        PassWord: req.body.PassWord,
+        idUser: req.body,idUser
+    }
+    var sql = 'insert into User set ?'
+    db.query(sql,data,(err,kq)=>{
+        if(err) throw err
+        console.log(kq)
+        res.send({
+            status: "Them thanh cong",
+            UserName: req.body.UserName,
+            PassWord: req.body.PassWord
+        })
+    })
+})
 
 app.listen(3000,'192.168.1.93',()=>{
     console.log("server dang chay nha")
